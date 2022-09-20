b54cd45e1f2ddc4a4074cd0f473a2f707dee2c81
GabiAmorim-lobo <111286589+undefined@users.noreply.github.com>
2022-09-20 01:29:32 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# atividadelwc
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+
+  lead = {
+    primeiroNome : "Julia",
+    sobrenome : "Vaidosa",
+    idade : 20,
+    email : "julialobo@gmail.com", 
+    rating : "Hot",
+    empresa : "Julia Vaidosa",
+    receitaAnual : "R$ 20.000,00",
+  };
+}
